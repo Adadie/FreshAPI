f8434075d7d0c4881384101438f797aa05ec1243
Adadie-Junior <junioradadie@gmail.com>
2020-08-20 15:19:45 +0300

    Add validation for sending data
    If applied, commit will add user registration validations
    Add sending post validation
    Add sending message validation
    Add login validation
    Add JSON packages file
    Add .babelrc file

---
+++ Validations/uservalidation.js
@@ -0,0 +1,22 @@
+const Joi = require('@hapi/joi')
+
+const regvalidation = data =>{
+    const schema = Joi.object({
+    Fname: Joi.string().min(3).required(),
+    Lname: Joi.string().min(3).required(),
+    Email: Joi.string().min(8).required().email(),
+    Password: Joi.string().min(8).required()
+});
+   return schema.validate(data);
+};
+
+const loginvalidation = data =>{
+    const schema = Joi.object({
+    Email: Joi.string().min(8).required().email(),
+    Password: Joi.string().min(8).required()
+});
+   return schema.validate(data);
+};
+
+module.exports.loginvalidation = loginvalidation;
+module.exports.regvalidation = regvalidation;
