f964e3926206cf07e9b99165561cd55a3beda0b4
Adadie-Junior <junioradadie@gmail.com>
2020-08-27 09:34:46 +0300

    Change dotenv import method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import 'dotenv/config';
+require ('dotenv/config');
 
 const app = express();
 app.use(bodyParser.json());
